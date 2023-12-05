3457074e200f5578c5ef27b1900fd20fe3c8372c
shawnkoon <shawn9405@gmail.com>
2023-12-05 00:05:35 -0800

    Add day2 part2 solution

--- 2023/src/day2/index.ts
+++ 2023/src/day2/index.ts
@@ -5,7 +5,7 @@ export const day2_part1 = async (): Promise<number> => {
   const MAX_RED = 12;
   const MAX_GREEN = 13;
   const MAX_BLUE = 14;
-  let lines = await readFileLines(`${__dirname}/input.txt`);
+  const lines = await readFileLines(`${__dirname}/input.txt`);
 
   for (let l of lines) {
     const gRes = l.split(':');
@@ -15,16 +15,17 @@ export const day2_part1 = async (): Promise<number> => {
       const moves = game.split(',');
 
       return moves.every((move) => {
-        const [count, color] = move.trim().split(' ');
+        const [countStr, color] = move.trim().split(' ');
+        const count = parseInt(countStr);
 
         if (color.toLowerCase() === 'red') {
-          return parseInt(count) <= MAX_RED;
+          return count <= MAX_RED;
         }
         if (color.toLowerCase() === 'green') {
-          return parseInt(count) <= MAX_GREEN;
+          return count <= MAX_GREEN;
         }
         if (color.toLowerCase() === 'blue') {
-          return parseInt(count) <= MAX_BLUE;
+          return count <= MAX_BLUE;
         }
       });
     });
@@ -32,6 +33,42 @@ export const day2_part1 = async (): Promise<number> => {
     if (isPossible) {
       sum += gNumb;
     }
+  }
+
+  return sum;
+};
+
+export const day2_part2 = async (): Promise<number> => {
+  const lines = await readFileLines(`${__dirname}/input.txt`);
+  let sum = 0;
+
+  for (let l of lines) {
+    const gRes = l.split(':');
+    const games = gRes[1].split(';');
+    let maxRed = 0;
+    let maxGreen = 0;
+    let maxBlue = 0;
+
+    games.forEach((game) => {
+      const moves = game.split(',');
+
+      moves.forEach((move) => {
+        const [countStr, color] = move.trim().split(' ');
+        const count = parseInt(countStr);
+
+        if (color.toLowerCase() === 'red') {
+          maxRed = count > maxRed ? count : maxRed;
+        }
+        if (color.toLowerCase() === 'green') {
+          maxGreen = count > maxGreen ? count : maxGreen;
+        }
+        if (color.toLowerCase() === 'blue') {
+          maxBlue = count > maxBlue ? count : maxBlue;
+        }
+      });
+    });
+
+    sum += maxRed * maxGreen * maxBlue;
   }
 
   return sum;
